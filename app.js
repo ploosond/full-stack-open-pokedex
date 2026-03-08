ee6a47be03ff619b43b259ee4a200d21f246cc71
Prajwol Devkota <prajwol.devkota016@gmail.com>
2026-03-08 15:42:37 +0100

    add pull request trigger workflows

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
-  res.send('1')
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
 })
 
-app.get('/health', (req, res) => {
-  res.send('ok')
+app.get('/version', (req, res) => {
+  res.send('1')
 })
 
 app.listen(PORT, () => {
